6641fce4ae8f8b23d1e2f9c4d4affdbb861b73a9
Igor Pachkin <altair1901@icloud.com>
2024-02-17 17:30:01 +0100

    feat: add model and Network

--- Neobis_iOS_WeatherApp/Service/Service.swift
+++ Neobis_iOS_WeatherApp/Service/Service.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class WeatherService {
+    let apiKey = "ab4b6ccab4d22e72f27428ed0b5d3c67"
+    
+    func fetchWeather(cityName: String, completion: @escaping (Weather) -> ()) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(apiKey)"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Data Task Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+
+            do {
+                let weather = try JSONDecoder().decode(Weather.self, from: data)
+                completion(weather)
+                print(weather)
+            } catch let jsonError {
+                print(jsonError)
+            }
+        }.resume()
+    }
+}
--- Neobis_iOS_WeatherApp/ViewModel/WeatherViewModel.swift
+++ Neobis_iOS_WeatherApp/ViewModel/WeatherViewModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+protocol WeatherViewModelType {
+    
+}
+
+class WeatherViewModel: WeatherViewModelType {
+    
+}
